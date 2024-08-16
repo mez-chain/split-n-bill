8ee5aa8cd37ac8d17dbb1e5e17b9237e1f82ddf5
N.MEZIANY <mez.blockchain@gmail.com>
2024-08-16 19:18:08 +0300

    added code to make the split form dynamic

--- src/splitForm.js
+++ src/splitForm.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
+
 export default function SplitForm() {
+  const [bill, setBill] = useState(0);
+  const [myExpense, setMyExpense] = useState(0);
+  // const [friendExpense, setFriendExpense] = useState(0);
+
   return (
     <form className="form-split-bill">
       <h2>split a bill with X</h2>
       <label>💰 Bill value</label>
-      <input type="text" />
+      <input type="text" onChange={(e) => setBill(Number(e.target.value))} />
       <label>🧍🏼‍♂️ Your expense</label>
-      <input type="text" />
+      <input
+        type="text"
+        onChange={(e) => setMyExpense(Number(e.target.value))}
+      />
       <label>🧑‍🤝‍🧑 X's expense</label>
-      <input type="text" />
+      <input type="text" value={bill - myExpense} readOnly />
       <label>🤑 Who is paying the bill?</label>
       <select value="You">
         <option value="You">You</option>
